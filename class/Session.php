371cf552c4a5924c0b2b7071be4d5838434af9e2
pguyon <pguyon69@gmail.com>
2021-06-03 15:01:48 +0200

    fin

--- class/Session.php
+++ class/Session.php
@@ -3,9 +3,24 @@ class Session
 {
     public $attributs = [];
 
+
+    public function __isset($key)
+    {
+        return isset($this->attributs[$key]);
+    }
+
+    public function __unset($key)
+    {
+        unset($this->attributs[$key]);
+    }
     public function __set($key, $value)
     {
         return $this->attributs[$key] = $value;
+    }
+
+    public function __get($key)
+    {
+        return $this->attributs[$key];
     }
 
     public function setAttributes()
